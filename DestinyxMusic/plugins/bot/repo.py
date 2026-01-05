b697275cd1fa7f78fec86ff9d19d8fc650d5120e
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-01-05 20:51:50 +0530

    Add files via upload

---
+++ DestinyxMusic/plugins/bot/repo.py
@@ -0,0 +1,42 @@
+﻿# Authored By Certified Coders © 2025
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from AnnieXMedia import app
+from config import BOT_USERNAME
+
+repo_caption = """**
+🚀 ᴄʟᴏɴᴇ ᴀɴᴅ ᴅᴇᴘʟᴏʏ – ᴄᴇʀᴛɪꜰɪᴇᴅ ᴄᴏᴅᴇʀꜱ ʀᴇᴘᴏ 🚀
+
+➤ ᴅᴇᴘʟᴏʏ ᴇᴀsɪʟʏ ᴏɴ ʜᴇʀᴏᴋᴜ ᴡɪᴛʜᴏᴜᴛ ᴇʀʀᴏʀꜱ  
+➤ ɴᴏ ʜᴇʀᴏᴋᴜ ʙᴀɴ ɪꜱꜱᴜᴇ  
+➤ ɴᴏ ɪᴅ ʙᴀɴ ɪꜱꜱᴜᴇ  
+➤ ᴜɴʟɪᴍɪᴛᴇᴅ ᴅʏɴᴏꜱ  
+➤ ʀᴜɴ 24/7 ʟᴀɢ ꜰʀᴇᴇ
+
+ɪꜰ ʏᴏᴜ ꜰᴀᴄᴇ ᴀɴʏ ᴘʀᴏʙʟᴇᴍ, ꜱᴇɴᴅ ꜱꜱ ɪɴ ꜱᴜᴘᴘᴏʀᴛ
+**"""
+
+@app.on_message(filters.command("repo"))
+async def show_repo(_, msg):
+    buttons = [
+        [InlineKeyboardButton("➕ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ✨", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+        [
+            InlineKeyboardButton("👑 ᴏᴡɴᴇʀ", url="https://t.me/CertifiedCoder"),
+            InlineKeyboardButton("💬 ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/CertifiedCodes")
+        ],
+        [
+            InlineKeyboardButton("🛠️ ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/CertifiedCoders"),
+            InlineKeyboardButton("🎵 ɢɪᴛʜᴜʙ", url="https://github.com/CertifiedCoders/AnnieXMusic")
+        ]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(buttons)
+
+    try:  
+        await msg.reply_photo(
+            photo="https://telegra.ph/file/58afe55fee5ae99d6901b.jpg",
+            caption=repo_caption,
+            reply_markup=reply_markup
+        )
+    except:
+        pass
