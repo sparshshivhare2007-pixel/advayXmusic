2220432d6ffc832b232b554d5fac1b05fd17b069
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2026-01-05 21:13:02 +0530

    Add files via upload

---
+++ DestinyxMusic/utils/inline/__init__.py
@@ -0,0 +1,8 @@
+﻿# Authored By Certified Coders © 2025
+from .extras import *
+from .help import *
+from .play import *
+from .queue import *
+from .settings import *
+from .start import *
+from .speed import *
---
+++ DestinyxMusic/utils/inline/stats.py
@@ -0,0 +1,54 @@
+﻿# Authored By Certified Coders © 2025
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+class StatsCallbacks:
+    SHOW_OVERVIEW = "stats:overview"
+    SHOW_BOT_STATS = "stats:bot"
+    BACK = "stats:back"
+    CLOSE = "stats:close"
+
+
+def build_stats_keyboard(_, is_sudo: bool) -> InlineKeyboardMarkup:
+    non_sudo_row = [
+        InlineKeyboardButton(
+            text=_["SA_B_1"],
+            callback_data=StatsCallbacks.SHOW_OVERVIEW,
+        )
+    ]
+    sudo_row = [
+        InlineKeyboardButton(
+            text=_["SA_B_2"],
+            callback_data=StatsCallbacks.SHOW_BOT_STATS,
+        ),
+        InlineKeyboardButton(
+            text=_["SA_B_3"],
+            callback_data=StatsCallbacks.SHOW_OVERVIEW,
+        ),
+    ]
+    rows = [
+        sudo_row if is_sudo else non_sudo_row,
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=StatsCallbacks.CLOSE,
+            )
+        ],
+    ]
+    return InlineKeyboardMarkup(rows)
+
+
+def build_back_keyboard(_) -> InlineKeyboardMarkup:
+    rows = [
+        [
+            InlineKeyboardButton(
+                text=_["BACK_BUTTON"],
+                callback_data=StatsCallbacks.BACK,
+            ),
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=StatsCallbacks.CLOSE,
+            ),
+        ]
+    ]
+    return InlineKeyboardMarkup(rows)
